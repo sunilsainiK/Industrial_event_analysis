e3f6e3c6b7826521489c9cf33807a26076e08dbf
Sunil Saini <sunil.saini@de.abb.com>
2019-04-01 16:52:49 +0200

    modification

--- Dash_industrial_event_log_analysis/Dash_industrial_event_log_analysis/Dash_industrial_event_log_analysis/pages/Preprocess.py
+++ Dash_industrial_event_log_analysis/Dash_industrial_event_log_analysis/Dash_industrial_event_log_analysis/pages/Preprocess.py
@@ -13,7 +13,7 @@ import re
 prep='prepackage=PreProcessing'
 r  = requests.get("http://127.0.0.1:5000/preprocess",params=prep)
 list =re.split(',',r.text)
-print (list)
+
 def button_values(list):
     i=1
     btn_values =[]
@@ -23,12 +23,19 @@ def button_values(list):
            btn_values.append(html.Div(html.Button(list[i] ,id='id_0_'+str(i) ,title=value, n_clicks=0, style={'width':'80%',
                                                                                    'margin-top':'4%',
                                                                                    'margin-left':'4%',
-                                                                                   'margin-bottom':'4%' }),
+                                                                                   'margin-bottom':'4%'}),
                                                                                    className="row"))
-           print (btn_values)
     return btn_values
 
 
+def info_prep(list):
+    i=1
+    btn = []
+    for i in range (len(list)):
+       if not  list[i]=='':
+           value=list[i]
+           btn.append(html.Div(html.Button('i' ,id=value ,title=value, n_clicks=0, style={'width':'1%','margin-top':'0.8%'})))
+    return btn
 
 layout = html.Div(
        html.Div([html.H1(children='Industrial Event Log Analysis',style={
@@ -36,23 +43,20 @@ layout = html.Div(
                                                                   'padding-right': '2.0%',
                                                                   'border':'solid',
                                                                   #'background-color':'DodgerBlue',
-                                                                  'text-align':'center'                                                                                  }
+                                                                  'text-align':'center'}
                  ),
                    html.Div([html.H2(children='Preprocessing',style={
                                                             'padding-left': '2.0%',
                                                             'padding-right': '2.0%',
                                                              'margin-top':'2%',
-
                                                              'border':'solid',
-                                                             #'background-color':'DodgerBlue',
                                                              'text-align':'center'
                                                                })
                                                                ]),
-                html.Div(html.Div(button_values(list)),
-#                         html.Div(info(list)),
-                         style={'width':'20%','margin-top':'2%','margin-left':'2%', 'border':'solid'}),
 
-
+                html.Div(html.Div(button_values(list),className="seven columns"),#                         html.Div(info(list)),
+                         style={'width':'20%','margin-top':'2%','margin-left':'2%'}),
+                         html.Div(info_prep(list)),
 
 
 
