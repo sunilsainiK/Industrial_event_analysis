c6d959bbd196892fa039b8712e21d2be139c9e60
Sunil Saini <sunil.saini@de.abb.com>
2019-04-02 07:08:18 +0200

    modification 2 april

--- Dash_industrial_event_log_analysis/Dash_industrial_event_log_analysis/Dash_industrial_event_log_analysis/pages/Preprocess.py
+++ Dash_industrial_event_log_analysis/Dash_industrial_event_log_analysis/Dash_industrial_event_log_analysis/pages/Preprocess.py
@@ -10,6 +10,10 @@ import requests
 import plotly.plotly as py
 from plotly import figure_factory as FF
 import re
+
+
+df = pd.read_csv('df_raw')
+
 prep='prepackage=PreProcessing'
 r  = requests.get("http://127.0.0.1:5000/preprocess",params=prep)
 list =re.split(',',r.text)
@@ -38,7 +42,8 @@ def info_prep(list):
     return btn
 
 layout = html.Div(
-       html.Div([html.H1(children='Industrial Event Log Analysis',style={
+       html.Div([
+                html.H1(children='Industrial Event Log Analysis',style={
                                                                   'padding-left': '2.0%',
                                                                   'padding-right': '2.0%',
                                                                   'border':'solid',
@@ -51,13 +56,38 @@ layout = html.Div(
                                                              'margin-top':'2%',
                                                              'border':'solid',
                                                              'text-align':'center'
-                                                               })
-                                                               ]),
+                                                               }),
+                            ]),
 
+
+
+            html.Div([
                 html.Div(html.Div(button_values(list),className="seven columns"),#                         html.Div(info(list)),
-                         style={'width':'20%','margin-top':'2%','margin-left':'2%'}),
-                         html.Div(info_prep(list)),
+                         style={'width':'30%','margin-top':'2%','margin-bottom':'2%','margin-left':'2%'}),
+                         html.Div(info_prep(list),style={'margin-bottom':'2%'}),
+                         ],
+                         style={'width':'20%','border':'solid','overflowY':'scroll'}),
+html.Div([
+        html.Div(
+             dash_table.DataTable(
+                                     id='table-filtering',
+                                     columns=[
+                                         {"name": i, "id": i} for i in (df.columns)
+                                     ],style_header={'fontWeight': 'bold'},
+                                     pagination_settings={
+                                         'current_page': 0,
+                                         'page_size': 5
+                                     },
+                                     pagination_mode='be',
 
+                                     filtering='be',
+                                     filtering_settings='',style_table={'overflowX': 'scroll'}
+                                 ),
+                                 className="five columns",
+                            style={'margin-top':'5', 'margin-left':'40'}
+                            ),
+                         ],
+                         className="rows")
 
 
               ]),
